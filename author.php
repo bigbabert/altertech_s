1064b4408fd626f2fc6a26b18ddebc8ea9efb11f
Bigbabert <bigbabert@gmail.com>
2015-04-21 15:07:49 +0200

    Version: 1.6.1
    #fixed posts list style in author page

--- author.php
+++ author.php
@@ -9,29 +9,26 @@
  */
 
   get_header(); ?>
-<div class="g-wide--3 g-medium--half">    <div class="container">
+	<div id="primary" class="content-area">
+			<header class="page-header">
+
 	<?php if ( function_exists('yoast_breadcrumb') ) {
   yoast_breadcrumb('<p id="breadcrumbs">','</p>');
 } ?>
-    </div>
-    
-<article style="border-bottom: 1px solid #ccc;" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<div  class="container gs-mrg-top" >
 <!-- This sets the $curauth variable -->
     <?php
     $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
     ?>
 
-    <h2 class="editorial-header__subtitle gs-top"><?php _e( 'Author', 'altertech_s' ); ?>: <?php echo $curauth->display_name; ?></h2>
-    <div class="container gs_left">
-<div class="g-wide--1 g-medium--half"><p class="xlarge"><?php _e( 'Website', 'altertech_s' ); ?>: </p><br><p><a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a></p></div>
-<div class="g-wide--3 g-wide--last g-medium--half g--last gs_brd_left"><p class="xlarge"><?php _e( 'Profile', 'altertech_s' ); ?>: </p><br><p><?php echo $curauth->user_description; ?></p></div>
-    </div>
-                        </div>
+    <h2 class="editorial-header__subtitle"><?php _e( 'Author', 'altertech_s' ); ?>: <?php echo $curauth->display_name; ?></h2>
+    
+<div class="g--half"><p class="xlarge"><?php _e( 'Website', 'altertech_s' ); ?>: </p><br><p><a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a></p></div>
+<div class="g--half g--last gs_brd_left"><p class="xlarge"><?php _e( 'Profile', 'altertech_s' ); ?>: </p><br><p><?php echo $curauth->user_description; ?></p></div>
     <div class="clearfix"></div>
-			<div  class="container gs_left" >
     <h2><?php _e( 'Posts by', 'altertech_s' ); ?><?php echo $curauth->display_name; ?>:</h2>
-                        </div>
+<div class="clearfix"></div>
+			</header><!-- .page-header -->
+			</div>
 <!-- The Loop -->
 
     <?php if ( have_posts() ) : ?>
@@ -57,7 +54,5 @@
 
 		<?php endif; ?>
 <!-- End Loop -->
-</article>
-</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
