a68623bc7f36c31917c3ace64c5808a4bd44bce4
Bigbabert <bigbabert@gmail.com>
2015-06-13 01:07:28 +0200

    Version 1.7.1
    #fix right overflow;
    #fix right margin;
    #fix comment textarea on mobile;

--- product-searchform.php
+++ product-searchform.php
@@ -6,9 +6,9 @@
  */
 ?>
 <form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
-			<div>
+			<div class="gs_search">
 				<input type="text" class="search" name="s" id="s" placeholder="<?php _e( 'Search for products', 'altertech_s' ); ?>" />
-	        <button type="submit" id="searchsubmit" ><img src="<?php echo get_template_directory_uri(); ?>/images/search.png"></button>
+	        <button type="submit" id="searchsubmit" ><i class="genericon genericon-search"></i></button>
 				<input type="hidden" name="post_type" value="<?php _e( 'product', 'altertech_s' ); ?>" />
 			</div>
 		</form>
--- searchform.php
+++ searchform.php
@@ -6,9 +6,9 @@
  */
 ?>
 <form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div>
+    <div class="gs_search">
             	<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label','altertech_s' ); ?></label>
                 <input type="text" class="search" placeholder="<?php esc_attr_e( 'Search', 'altertech_s' ); ?>" name="s" id="s" />
-	        <button type="submit" id="searchsubmit" ><img src="<?php echo get_template_directory_uri(); ?>/images/search.png"></button>
+	        <button type="submit" id="searchsubmit" ><i class="genericon genericon-search"></i></button>
         </div>
 </form>
